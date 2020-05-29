b27135ebd8ca41165b783ce9fccf3f0628d9520e
DHatziioanou <DHatziioanou@users.noreply.github.com>
2020-05-29 12:47:00 +0100

    Allow use with any database
    Adapted for use with any database containing a postcode column

--- index_postcode.R
+++ index_postcode.R
@@ -1,25 +1,33 @@
-#' Index ONS postcode database
+#' Create an Index for a postcode database
 #'
-#' Creates an index of ONS postcode files ‘https://www.ons.gov.uk/‘
+#' Creates an index of a postcode database file using the first letters and first number from within the postcode. This has been designed for use with UK postcodes and may not work for postcodes which do not start with letters followed by numbers.
 #'
-#' @param postcode_path path to folder containing ONS postcode multi_csv files. Eg for ONS NSPL this is file.path("NSPL_FEB_2019_UK", "Data", "multi_csv") from where the ONS data was extracted
+#' @param postcode_path path to folder containing mulitple postcode csv files or single csv file. Eg for ONS NSPL this could be either file.path("NSPL_FEB_2019_UK", "Data", "multi_csv") from where the ONS data was extracted if using the multi_csv files or file.path("NSPL_FEB_2019_UK", "Data", "NSPL_FEB_2019_UK.csv") if using the full database file. Using multi_csv files can overcome insufficient memory barriers.
 #'
-#' @return Returns an index of ONS postcode files using the first letters from the file names and the first digits of the numbers within the postcodes of each file as a data.frame object. Saves the index file to the folder above the postcode file location.
+#' @param colname column name within database csv file(s) with postcode for postcode matching (optional). Default is the first column name within the database.
+#'
+#' @param name name to call the index (optional). Default is "Index".
+#'
+#' @param exclude string pattern within files to exclude when using a folder as postcode_path (optional).
+#'
+#' @return Returns a postcode database Index using the first letters from the file names and the first digits of the numbers within the postcodes as a data.frame object. Saves the Index file to the location of the postcode database.
 #'
 #'
 #' @author Diane Hatziioanou
 #'
-#' @keywords postcode, ONS, NSPL
+#' @keywords postcode, Index
 #'
 #' @examples
 #' Index <- index_postcodes(postcode_path)
 #'
 #' Index_ONS <- index_postcodes("C:/.../NSPL_FEB_2019_UK/Data/multi_csv/)
 #'
-#' Index_ONS <- index_postcodes(file.path(path, "NSPL_FEB_2019_UK, "Data ,"multi_csv"))
+#' Index_ONS <- index_postcodes(file.path(path, "NSPL_FEB_2019_UK, "Data ,"multi_csv"), exclude = "Readme.csv")
+#'
+#' Index_file <- index_postcodes(file.path(path, "2020 database, "Reference.csv"), colname = "pcd", name = "Reference_index")
 #'
 #' @export
-index_postcodes <- function(postcode_path){
+index_postcodes <- function(postcode_path, colname, name, exclude){
 start_time <- Sys.time()
 if (!require("data.table")) {
   install.packages("data.table")
@@ -30,74 +38,129 @@ if (!require("dplyr")) {
   library(dplyr)
 }
 
-# List files
-ONS_files <- list.files(path = file.path(postcode_path),
-                   include.dirs = FALSE,
-                   recursive = FALSE,
-                   pattern = ".csv")
-ONS_files <- as.data.frame(ONS_files[!grepl("\\$", ONS_files)], responseName  = "file", row.names = NULL, optional = F, stringsAsFactors =F)
-data.table::setnames(ONS_files, "file")
+if (missing(name)) {
+  name <- "Index"
+}
 
-setDT(ONS_files)[, full_path := file.path(postcode_path,ONS_files$file)]
-ONS_files[, Pc := (gsub(".*UK_(.+).csv*", "\\1",ONS_files$file))]
+# Database file path and file name(s)
+if (dir.exists(postcode_path)) { # Directory
 
-# Get file row indexes positions
-for(i in 1:nrow(ONS_files)){
+  # List files
+  Db_files <- list.files(path = file.path(postcode_path),
+                         include.dirs = F,
+                         recursive = F,
+                         pattern = ".csv")
+  Db_files <- as.data.frame(Db_files[!grepl("\\$", Db_files)], responseName  = "file", row.names = NULL, optional = F, stringsAsFactors = F)
+  data.table::setnames(Db_files, "file")
 
-# Get file index numbers
-Pc <- fread(file.path(ONS_files[i,"full_path"]), header = T)
-Pc[,N1 := ""]
-Pc[,N1 := gsub(paste0(".*",ONS_files[i,3],"([0-9]+).*$"), "\\1", Pc$pcd)]
-Pc[,N1 := factor(substr(start = 1, stop = 1, x= Pc$N1))]
-Pc[,row := 1:nrow(Pc)]
+  setDT(Db_files)[, full_path := file.path(postcode_path,Db_files$file)]
+  Db_files <- Db_files[!file.info(Db_files$full_path)$isdir]
+
+  if (missing(exclude)) {
+    message(paste("Indexing all csv files within", postcode_path, "as ONE database"))
+  } else {
+
+    Db_files <- Db_files[!grepl(paste(exclude, collapse = "|") , Db_files$file),]
+    message(paste("Indexing", NROW(Db_files$file), "csv files within", postcode_path, "as ONE database"))
+
+  }
+  if (dim(Db_files)[1] == 0) {
+
+    stop("No csv files found.")
+
+  }
+
+
+
+  # Check database files
+    for(i in 1:nrow(Db_files)) {
+
+    db_file <- fread(file.path(Db_files[i,"full_path"]), header = F, nrows = 1)
+    if(exists("db_files")) {
+      t <- try(db_files <- rbind(db_files, db_file, stringsAsFactors = F), silent = T)
+      ifelse(inherits(t, "try-error"), stop("Column name mismatch; csv files don't all belong to the same database."),t)
+
+    } else {
+      db_files <- db_file
+    }
+    }
+
+  # Postcode column
+  if (missing(colname)) {
+
+    colname <- db_files[1,1] %>% as.character()
+
+  }
+  remove(db_files, db_file)
+  path <- postcode_path
+
+} else if (file.exists(postcode_path) && !dir.exists(postcode_path)) { # Single file
+
+  Db_files <- postcode_path
+  Db_files <- as.data.frame(Db_files[!grepl("\\$", Db_files)], responseName  = "file", row.names = NULL, optional = F, stringsAsFactors =F)
+  data.table::setnames(Db_files, "full_path")
+  setDT(Db_files)[, file := basename(postcode_path)]
+  message(paste("Indexing csv file", Db_files$file, "within", postcode_path, "as ONE database"))
+  path <- dirname(postcode_path)
+
+  # Postcode column
+  if (missing(colname)) {
+    colname <- fread(file.path(Db_files[1,"full_path"]), header = F, select = 1, nrows = 1) %>% as.character()
+  }
+
+} else { # Missing database error
+
+  stop("Could not find any .csv files at ", postcode_path)
+
+}
+
+
+# Get file row index positions
+for(i in 1:nrow(Db_files)) {
+
+# Get postcode letters and first number
+Pc <- fread(file.path(Db_files[i,"full_path"]), header = T)
+
+setDT(Pc)[,block := toupper(gsub(" ", "", Pc[,get(colname)]))]
+Pc[, block := (gsub('([a-zA-Z])([0-9])', '\\1_\\2', block))]
+Pc[, block := paste0(gsub( "_.*$", "",  block), substr(gsub('\\D+','', block), 1,1))]
+Pc[, block := factor(Pc$block)]
+Pc[, row := 1:nrow(Pc)]
 
 # Get file index rows
-rows <- base::split(1:nrow(Pc), Pc$N1)
+rows <- base::split(1:nrow(Pc), Pc$block)
 index <- sapply(rows, function(rowi) {rowi[which.min(Pc$row[rowi])]})
 index <- setDT(list(index))[] %>% setnames("start")
 index$stop <-  sapply(rows, function(rowi) {rowi[which.max(Pc$row[rowi])]})
-index[, Pc_N := names(rows)]
+index[, block := names(rows)]
 remove(rows)
-index[, Pc := ONS_files[i,3]]
 
-setDT(index)[, Pc_s := paste0(Pc,Pc_N)]
 index[,start := index$start + 1] # count for header row
 index[,stop := index$stop + 1] # count for header row
 
+index[,full_path := Db_files[i,"full_path"]]
+index[,file := Db_files[i,"file"]]
+
 # IF MERGED DATASET EXISTS, MERGE
-if (exists("Index") & dim(index)[1] !=0 ){
-  Index <-rbind(Index, index, fill=TRUE, stringsAsFactors = FALSE)
+if (exists("Index") & dim(index)[1] != 0 ) {
+  Index <- rbind(Index, index, fill = T, stringsAsFactors = F)
 }
 # IF MERGED DATASET DOESN'T EXIST, CREATE IT
-if (!exists("Index")){
+if (!exists("Index")) {
   Index <- index
 }
 remove(index,Pc)
-
 }
-
-setDT(Index)[,Pc:=factor(Index$Pc)]
-
-
-# Add Index to file list
-Index <- merge(ONS_files,Index, all =T, by = "Pc")
-
-Index[,c("Pc", "Pc_N"):= NULL]
-setcolorder(Index,c("Pc_s", "start", "stop", "file", "full_path"))
-
 
 # Save index
-fwrite(x = Index, file.path(dirname(postcode_path), "Index.csv"), row.names = F, nThread = getDTthreads())
+setcolorder(Index,c("block", "start", "stop", "file", "full_path"))
+fwrite(x = Index, file.path(path, paste0(name,".csv")), row.names = F, nThread = getDTthreads())
+
+# Report
 end_time <- Sys.time()
 message(paste("Run time:",end_time - start_time))
-message(paste("Index saved at:",file.path(dirname(postcode_path), "Index.csv")))
+message(paste("Index saved at:",file.path(path, paste0(name,".csv"))))
 return(Index)
+
 }
-
-
-
-
-
-
-
 
